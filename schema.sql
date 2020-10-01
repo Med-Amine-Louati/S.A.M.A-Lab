81b74a51af697f6c88780f86fc903beddf3d8453
Med-Amine-Louati <medamine505@live.fr>
2020-10-01 11:49:28 +0100

    new schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
 DROP DATABASE IF EXISTS project;
-
 CREATE DATABASE project;
-
 USE project;
+
 
 CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
@@ -10,6 +9,9 @@ CREATE TABLE users (
   first_name varchar(255) NOT NULL UNIQUE,
   last_name varchar(255) NOT NULL UNIQUE,
   password varchar(255) NOT NULL ,
+  email varchar(255) NOT NULL UNIQUE,
+  role varchar(255) NOT NULL,
+  privilege varchar(255) NOT NULL,
   PRIMARY KEY (ID)
 );
  CREATE TABLE organizations (
@@ -19,23 +21,79 @@ CREATE TABLE users (
    field varchar(100) NOT NULL,
    serial int(4) NOT NULL UNIQUE,
    userID int NOT NULL,
-   PRIMARY KEY (ID),
-   FOREIGN KEY (userID) REFERENCES users(id)
+   FOREIGN KEY (userID) REFERENCES users(id),
+    PRIMARY KEY (ID)
 );
+
 CREATE TABLE projects (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(50) NOT NULL,
   description varchar(250),
   organizationID int NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (organizationID) REFERENCES organizations(id)
+  FOREIGN KEY (organizationID) REFERENCES organizations(id),
+  PRIMARY KEY (ID)
 );
- CREATE TABLE feeds (
+
+CREATE TABLE Privileges(
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (ID)
+);
+CREATE TABLE Privll_User(
    id int NOT NULL AUTO_INCREMENT,
-   name varchar(50) NOT NULL UNIQUE,
+  userID int NOT NULL,
+  privID int NOT NULL,
+  FOREIGN KEY (userID) REFERENCES Users(id),
+  FOREIGN KEY (userID) REFERENCES Privileges(id),
+  PRIMARY KEY (ID)
+);
+
+
+
+CREATE TABLE Issues(
+     id int NOT NULL AUTO_INCREMENT,
+   title varchar(50) NOT NULL UNIQUE,
    description varchar(250) NOT NULL,
-   type varchar(50) NOT NULL,
+   postedID int NOT NULL,
    projectID int NOT NULL,
-   PRIMARY KEY (ID),
-   FOREIGN KEY (projectID) REFERENCES projects(id)
+   FOREIGN KEY (projectID) REFERENCES projects(id),
+   FOREIGN KEY (postedID) REFERENCES users(id),
+   state varchar(50) NOT NULL,
+   PRIMARY KEY (ID)
+);
+
+
+
+CREATE TABLE Feachers(
+     id int NOT NULL AUTO_INCREMENT,
+   title varchar(50) NOT NULL UNIQUE,
+   description varchar(250) NOT NULL,
+   postedID int NOT NULL,
+   projectID int NOT NULL,
+   FOREIGN KEY (projectID) REFERENCES projects(id),
+   FOREIGN KEY (postedID) REFERENCES users(id),
+   state varchar(50) NOT NULL,
+   PRIMARY KEY (ID)
+   
+);
+
+CREATE TABLE Comments(
+  id int NOT NULL AUTO_INCREMENT,
+  text varchar(250) NOT NULL,
+   userID int NOT NULL,
+   issueID int NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(id),
+   FOREIGN KEY (issueID) REFERENCES issues(id),
+   createdAt datetime NOT NULL,
+PRIMARY KEY (ID)
+);
+
+CREATE TABLE Messages (
+  id int NOT NULL AUTO_INCREMENT,
+  text varchar(250) NOT NULL,
+  senderID int NOT NULL,
+  receiverID int NOT NULL,
+  FOREIGN KEY (senderID) REFERENCES users(id),
+  FOREIGN KEY (receiverID) REFERENCES users(id),
+  PRIMARY KEY (ID)
 );
